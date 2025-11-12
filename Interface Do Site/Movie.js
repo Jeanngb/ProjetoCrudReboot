860f4ae9bc980a66031f6af4473690bd4a129964
pdroGab87 <ped.gabriel0609@gmail.com>
2025-11-12 18:08:01 -0300

    nova lista de filmes
    Removed old movie entries and added a new movie object with details.

--- Interface Do Site/Movie.js
+++ Interface Do Site/Movie.js
@@ -1,93 +1,50 @@
-const movies = [
-    {
-        id: 1,
-        title: "O Senhor dos Anéis: A Sociedade do Anel",
-        image: "../Imagens/SDA.jpg",
-        link: "#"
-    },
-    {
-        id: 2,
-        title: "Matrix",
-        image: "../Imagens/Matrix.jpg",
-        link: "#"
-    },
-    {
-        id: 3,
-        title: "Inception",
-        image: "../Imagens/Inception.jpg",
-        link: "#"
-    },
-    {
-        id: 4,
-        title: "O Rei Leão",
-        image: "../Imagens/OREI.jpg",
-        link: "#"
-    },
-    {
-        id: 5,
-        title: "Interestelar",
-        image: "../Imagens/Interestelar.jpg",
-        link: "#"
-    },
-    
-    {
-        "id": 1,
-        "title": "Inception",
-        "image": "../Imagens/Inception.jpg",
-        "link": "#"
-    },
-    {
-        "id": 2,
-        "title": "The Dark Knight",
-        "image": "../Imagens/TheDarkKnight.jpg",
-        "link": "#"
-    },
-    {
-        "id": 3,
-        "title": "Interstellar",
-        "image": "../Imagens/Interstellar.jpg",
-        "link": "#"
-    },
-    {
-        "id": 4,
-        "title": "The Matrix",
-        "image": "../Imagens/TheMatrix.jpg",
-        "link": "#"
-    },
-    {
-        "id": 5,
-        "title": "The Shawshank Redemption",
-        "image": "../Imagens/ShawshankRedemption.jpg",
-        "link": "#"
-    },
-    {
-        "id": 6,
-        "title": "Fight Club",
-        "image": "../Imagens/FightClub.jpg",
-        "link": "#"
-    },
-    {
-        "id": 7,
-        "title": "The Godfather",
-        "image": "../Imagens/TheGodfather.jpg",
-        "link": "#"
-    },
-    {
-        "id": 8,
-        "title": "Pulp Fiction",
-        "image": "../Imagens/PulpFiction.jpg",
-        "link": "#"
-    },
-    {
-        "id": 9,
-        "title": "The Lion King",
-        "image": "../Imagens/TheLionKing.jpg",
-        "link": "#"
-    },
-    {
-        "id": 10,
-        "title": "Forrest Gump",
-        "image": "../Imagens/ForrestGump.jpg",
-        "link": "#"
-    }// Adicione mais filmes conforme necessário
+/*RANKING:
+
+The Shawshank Redemption (1994) - 4.62
+The Godfather (1972) - 4.58
+The Lord of the Rings: The Return of the King (2003) - 4.57
+12 Angry Men (1957) - 4.57
+The Godfather: Part II (1974) - 4.55
+The Dark Knight (2008) - 4.53
+Schindler’s List (1993) - 4.52
+The Lord of the Rings: The Fellowship of the Ring (2001) - 4.50
+The Good, the Bad and the Ugly (1966) - 4.50
+The Lord of the Rings: The Two Towers (2002) - 4.47
+City of God (2002) - 4.47
+Seven Samurai (1954) - 4.46
+The Empire Strikes Back (1980) - 4.45
+Goodfellas (1990) - 4.45
+Grave of the Fireflies (1989) - 4.41
+Harakiri (1962) - 4.41
+Come and See (1987) - 4.41
+Parasite (2019) - 4.40
+Interstellar (2014) - 4.38
+Terminator 2: Judgment Day (1991) - 4.38
+Fight Club (1999) - 4.38
+Spirited Away (2001) - 4.38
+One Flew Over the Cuckoo’s Nest (1975) - 4.38
+Cinema Paradiso (1990) - 4.38
+The Pianist (2002) - 4.37
+It’s a Wonderful Life (1946) - 4.37
+Pulp Fiction (1994) - 4.36
+Ikiru (1956) - 4.36
+Whiplash (2014) - 4.35
+Forrest Gump (1994) - 4.35
+*/
+const filmes = [
+  {
+    id: 1,
+    nome: "Homem-Aranha: Através do Aranhaverso",
+    sinopse:
+      "Após se reencontrar com Gwen Stacy, o amigável Homem-Aranha da vizinhança, morador do Brooklyn, é catapultado através do Multiverso, onde encontra a Sociedade Aranha, uma equipe de Homens-Aranha encarregada de proteger a própria existência do Multiverso. Mas quando os heróis entram em conflito sobre como lidar com uma nova ameaça, Miles se vê em conflito com os outros Homens-Aranha e precisa seguir sozinho para salvar aqueles que mais ama.",
+    capaPrincipal:
+      "https://a.ltrbxd.com/resized/film-poster/4/9/7/6/3/1/497631-spider-man-across-the-spider-verse-0-1000-0-1500-crop.jpg?v=f2acbf1b8a",
+    capaFundo: "img/i730374.jpeg",
+    anoLancamento: 2023,
+    diretor: "Justin K. Thompson e Kemp Powers",
+    genero: "Ação",
+    nota: 4.4,
+    trailer: "https://www.youtube.com/embed/_4is7I_ZxTg?si=GTWv2BmGFNCHRPnh",
+    streaming: ["NETFLIX"],
+  },
 ];
